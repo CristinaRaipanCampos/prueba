40d3200a671c866fe88d52260fc9600b4735f0af
Cristina Raipan Campos <craipan9888@e-camp.cl>
2022-01-20 15:21:37 -0300

    commit ultimo avance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
 /* funcion de inicio (funtion ready) */
 $(function () {
+    /* funcion que realiza el scroll lento */
+    $("a").click( function() {
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top - 100
+          }, 1000, function(){
+            window.location.hash = hash;
+          });
+       });
     /* para mostrar mensaje cuando se pasa el cursor sobre el boton */
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     });
-    $("a").click(function () {
-        var enlace = this.hash;
-        console.log(enlace)
-        $("html, body").animate(
-            {
-                scrolltop: $(enlace).offset().top - 89
-            }, 
-            3000
-        )
+    /* funcion para desaparecer el text area */
+    $( "#boton-form" ).click(function() {
+        $( ".form-control" ).toggle(500);
     });
+    $( "#cerrar" ).click(function() {
+        $( ".form-control" ).toggle(500);
+    });
+    /* $("#cerrar").click(function(){
+        $(".form-control").show(3000,function(){})
+    }) */
 });
-
-
-
-

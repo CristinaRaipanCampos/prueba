a2736e4912bf2c03a18710d2b1d07160f7746e20
Cristina Raipan Campos <craipan9888@e-camp.cl>
2022-01-20 22:09:13 -0300

    se agrego funcionalidad al modal y al form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 /* funcion de inicio (funtion ready) */
 $(function () {
     /* funcion que realiza el scroll lento */
-    $("a").click( function() {
-          var hash = this.hash;
-          $('html, body').animate({
+    $("a").click(function () {
+        var hash = this.hash;
+        $('html, body').animate({
             scrollTop: $(hash).offset().top - 100
-          }, 1000, function(){
+        }, 1000, function () {
             window.location.hash = hash;
-          });
-       });
+        });
+    });
     /* para mostrar mensaje cuando se pasa el cursor sobre el boton */
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     });
     /* funcion para desaparecer el text area */
-    $( "#boton-form" ).click(function() {
-        $( ".form-control" ).toggle(500);
+    $("#boton-form").click(function () {
+        $(".form-control").toggle(500);
     });
-    $( "#cerrar" ).click(function() {
-        $( ".form-control" ).toggle(500);
+    $("#cerrar").click(function () {
+        $(".form-control").toggle(500);
     });
-    /* $("#cerrar").click(function(){
-        $(".form-control").show(3000,function(){})
-    }) */
+    /* muestra mensaje en el modal */
+    $("#boton-form").click(function () {
+        //obtener valor usando jquery
+        var nombreFromInput = $("#nombre").val();
+        document.getElementById('mostrarNombre').innerHTML = nombreFromInput;
+        //obtener valor usando javaScript
+        var asuntoFromInput = document.getElementById("asunto").value;
+        document.getElementById('mostrarAsunto').innerHTML = asuntoFromInput;
+    })
+    /* limpia los campos del formulario */
+    $('#cerrar').click(function () {
+        /* document.getElementById("nombre").value = "";
+        document.getElementById("asunto").value = ""; */
+        $('#nombre').val("");
+        $('#asunto').val("");
+    });
+
+
 });
